ebe1cbe96e428055923ae742286a573e75784630
SimilArity <stein.william.be@gmail.com>
2022-05-10 21:16:11 +0200

    2/4 Boxes Challenge OK

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import {useState} from 'react';
+import boxes from './boxes';
+import Box from './Box';
 import './styles.css';
 
 function App() {
 
+  const [squares, setSquares] = useState(boxes);
+
+  const squareElements = squares.map((square)=>(
+    <Box key={square.id} on={square.on}/>
+  ))
 
   return (
     <div className="container">
-         
+        {squareElements}
     </div>
   );
 }
---
+++ src/Box.js
@@ -0,0 +1,12 @@
+export default function Box (props) {
+
+    const styles = {
+        backgroundColor: props.on ? "black" : "none"
+    }
+
+    return (
+        <>
+            <div className="box" style={styles}></div>
+        </>
+    )
+}
