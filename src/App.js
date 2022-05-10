9d6ba0279d67d59eece2a96281858e7d945c9103
SimilArity <stein.william.be@gmail.com>
2022-05-10 23:10:35 +0200

    4/4

--- src/App.js
+++ src/App.js
@@ -7,8 +7,16 @@ function App() {
 
   const [squares, setSquares] = useState(boxes);
 
+  function toggle(id){
+    setSquares(prevSquares=>{
+      return prevSquares.map((square)=>{
+        return square.id === id ? {...square, on: !square.on} : square
+      })
+    })
+  }
+
   const squareElements = squares.map((square)=>(
-    <Box key={square.id} on={square.on}/>
+    <Box key={square.id} id={square.id} on={square.on} handleClick={toggle}/>
   ))
 
   return (
--- src/Box.js
+++ src/Box.js
@@ -1,12 +1,18 @@
 export default function Box (props) {
 
+    //const [on, setOn] = useState(props.on)
+
     const styles = {
-        backgroundColor: props.on ? "black" : "none"
+        backgroundColor: props.on ? "black" : "transparent"
     }
+
+    /*function toggle () {
+        setOn (prevOn => !prevOn)
+    }*/
 
     return (
         <>
-            <div className="box" style={styles}></div>
+            <div className="box" style={styles} onClick={()=>props.handleClick(props.id)}></div>
         </>
     )
 }
